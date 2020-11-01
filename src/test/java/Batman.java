065198821d5f1eeabaa789b9e44f192a165200c0
tenus88 <diachenkoandrei1@gmail.com>
2020-11-01 11:24:05 -0500

    Batman has a car

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -4,5 +4,6 @@ public class Batman {
         System.out.println("I will catch you Joker");
         //Yse he will catch him
         //Batman has cool car
+        //Batman has cool motocycle
     }
 }
